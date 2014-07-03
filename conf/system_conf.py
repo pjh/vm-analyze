d2937b17227cade4cb250c5654abb511315fb63a
Peter Hornyack <pjh@cs.washington.edu>
2014-07-03 10:59:32 -0700

    Updated system_conf for stjohns

--- conf/system_conf.py
+++ conf/system_conf.py
@@ -5,34 +5,74 @@
 # Directory locations that may change depending on the system that we're
 # running on:
 
-system = 'verbena'
+system = 'stjohns'
 
-home_dir		= '/home/pjh'   # must be absolute!
+home_dir		= '/scratch/pjh'   # must be absolute!
 PERF_DIR		= "{}/bin".format(home_dir)
-repos_dir       = "{}/research".format(home_dir)
+repos_dir		= "{}".format(home_dir)
 scripts_dir		= "{}/vm-analyze".format(repos_dir)
-conf_dir        = "{}/conf".format(scripts_dir)
+conf_dir		= "{}/conf".format(scripts_dir)
 apps_dir		= "{}/vmstudy-apps".format(repos_dir)
-data_dir        = "{}/vmstudy-data".format(repos_dir)
-LTRACE_DIR      = "{}/ltrace_outfiles".format(data_dir)
-
-suggested_tracebuf_size_kb = 512 * 1024
+data_dir		= "{}/vmstudy-data".format(repos_dir)
+LTRACE_DIR		= "{}/ltrace_outfiles".format(data_dir)
 
 trace_user = 'pjh'
-trace_group = 'pjh'
+trace_group = 'grad_cs'
   # After the trace is complete, the owner:group of the tracefilename
   # output file will be set using these. Also applies to perf files.
+
+# stjohns: Intel(R) Xeon(R) CPU L5640  @ 2.27GHz
+#   http://ark.intel.com/products/47926
+#   https://en.wikipedia.org/wiki/List_of_Intel_Xeon_microprocessors#.22Westmere-EP.22_.2832_nm.29_Efficient_Performance
+#   According to /sys/devices/system/cpu/cpu0/cache/index0/shared_cpu_list,
+#   stjohns has 12 cores with 2 hyperthreads per core (hyperthreads share
+#   level-1 caches, it seems); private L1 + L2 caches per core (shared
+#   amongst the two hyperthreads), and two L3 caches shared by 6
+#   cores (12 threads) each.
+# I ran some small graph500 benchmarks (omp-csr/omp-csr -V -s 18)
+# with tracing on, and found that the trace infrastructure has one
+# buffer PER-HARDWARE-THREAD, not per-core! So on stjohns, the trace
+# lines come from cores 000 up to 023. This is somewhat unfortunate,
+# as it means that in order to increase the per-thread buffer size,
+# we end up using twice as much total memory. SOLUTION: use the
+# max_cpus=n kernel boot argument to limit the number of CPUs to
+# just 4! Otherwise, we need to use half of the system's memory
+# just to get 500 MB trace buffers per hw thread. With max_cpus set,
+# I verified that the kernel trace infrastructure only uses (and
+# presumably only allocates trace buffer memory for) four cores.
+memtotal = 24676440   # kB
+num_cores = 4
+num_hw_threads = 4
+suggested_tracebuf_size_kb = 1048576  # 1 GB
+#suggested_tracebuf_size_kb = 2097152  # 2 GB
+  # 2 GB memory per-core still leaves 23.5 - 8 = 15.5 GB memory
+  # for application use. However, the kernel may complain when
+  # we try to set this as the buffer_size_kb for tracing: 
+  #   "bash: echo: write error: Cannot allocate memory"
+  # Possible solution: set this as the trace_buf_size at boot
+  # time using boot parameter?!?
+'''
+num_cores = 12
+num_hw_threads = 24
+suggested_tracebuf_size_kb = int((memtotal / num_hw_threads) / 2)
+  # 50% of per-hw-thread memory: 23.5 GB memory -> 1004 MB per hw
+  # thread -> 502 MB trace buffer per thread. I'd like to have more
+  # memory per thread to avoid the trace buffer overflowing, but
+  # for now this is probably as much as we're going to reasonably
+  # get; now just 11.75 GB is left for application use :-/
+'''
 
 libreoffice_bin = '/usr/lib/libreoffice/program/soffice.bin'
   # Use soffice.bin directly, rather than the symlinks and wrapper scripts
   # around it.
 
 oldrepo_dir     = "{}/virtual-uw".format(repos_dir)
-glibc_dir		= "{}/glibc-testinstall/lib".format(oldrepo_dir)
-gcc_dir			= "{}/gcc-testinstall/lib64".format(oldrepo_dir)
+glibc_dir		= '/noexist'
+gcc_dir			= '/noexist'
 
 # These are used for determining whether or not files should count
-# as shared library files.
+# as shared library files.  If these directories don't actually exist
+# on some system, shouldn't be a problem.
 # NOTE: using /usr/lib here may be more "permissive" than we eventually
 # want - "application-specific" libraries used specifically for apache,
 # php, etc. will be included as "shared libraries"!
